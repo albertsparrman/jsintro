7010b523752e8754846b1aa46f133fb113c2c313
Albert <albert.sparrman@gmail.com>
2023-09-05 14:26:59 +0200

    Jag variablade

---
+++ variabler.js
@@ -0,0 +1,15 @@
+let name = 'Greg'
+console.log(typeof(name))
+const greeting = `Hej ${name}`
+console.log(greeting)
+let x = Number.MAX_VALUE
+console.log(name+x*2)
+let y = x * 2 +1
+console.log(typeof(y))
+console.log(isNaN(y))
+
+function CL (val) {
+    console.log(val)
+}
+
+CL ("beans")
